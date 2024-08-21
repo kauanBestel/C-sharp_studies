0662025a6893f39f704f399cb069288ce354ead6
KauanBestel <kauan.bestell@gmail.com>
2024-08-21 09:01:59 -0300

    enum studies

---
+++ Classes/enum2.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace HelloWorld
+{
+    enum Months
+    {
+        janeiro,
+        fevereiro,
+        marco,
+        abril,
+
+    }
+
+     class Program1{
+     static void Main(string[] args){
+
+     }
+    }
+}
